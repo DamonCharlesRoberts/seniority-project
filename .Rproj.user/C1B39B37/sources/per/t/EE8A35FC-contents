ff8297a390cf9888cb6528dc223cef9a45fa02dc
Damon C. Roberts <55409288+DamonCharlesRoberts@users.noreply.github.com>
2020-12-09 21:54:42 -0700

    draft2

--- .Rproj.user/C1B39B37/sources/per/t/EE8A35FC-contents
+++ .Rproj.user/C1B39B37/sources/per/t/EE8A35FC-contents
@@ -9,13 +9,17 @@
 
 #### Sourcing ####
 here::here()
-source('code/setup-file.R')
+source('code/1_setup-file.R')
 
 
 #### Funciton Execution ####
 setup()
 brookingsHOR <- loadBrookingshor()
+brookingsHOR <- brookingsHOR %>%
+  mutate(terms = (MeanTerm)/2)
 brookingsSEN <- loadBrookkingssen()
+brookingsSEN <- brookingsSEN %>%
+  mutate(terms= (MeanYears/6))
 
 #### Graphic Creation ####
 
@@ -25,7 +29,7 @@ horPlot <- ggplot(brookingsHOR, aes(x=Congress,y=MeanTerm)) +
   ggtitle("Mean Rep. Term Length (Years) Across Congresses")
 horPlot
 ggsave('figures/rep-agg-plot.png')
-senPlot <- ggplot(brookingsSEN, aes(x=Congress,y=MeanYears)) +
+senPlot <- ggplot(brookingsSEN, aes(x=Congress,y=stdmeanyears)) +
   geom_line( color="#1565C0", size=2, alpha=0.9, linetype=2) +
   theme_ipsum() +
   ggtitle("Mean Sen. Term Length (Years) Across Congresses")
@@ -34,18 +38,18 @@ ggsave('figures/sen-agg-plot.png')
 
 termannotation <- data.frame(
   x = c(115, 85),
-  y = c(6.5, 11.5),
+  y = c(3.5, 2),
   label = c('House', 'Senate')
 )
 termtimeplot <- ggplot() +
-  geom_line(data = brookingsHOR, aes(x = Congress, y = MeanTerm),color="#000000", size=2, alpha=0.9, linetype=3) +
-  geom_line(data = brookingsSEN, aes(Congress, y=MeanYears), color="#000000", size=2, alpha=0.9, linetype=4) + 
+  geom_line(data = brookingsHOR, aes(x = Congress, y = terms),color="#000000", size=2, alpha=0.9, linetype=1) +
+  geom_line(data = brookingsSEN, aes(Congress, y=terms), color="#000000", size=2, alpha=0.9, linetype=2) + 
   theme_tufte() +
   ggtitle("Figure 1. Average MC Term-Length Over Congressional Sessions") +
   geom_text(data = termannotation, aes(x=x,y=y,label=label),
             color = '#000000', size = 5, frontface = 'bold') +
-  labs(caption = "Damon Roberts | Data Source: Brookings Institute Vital Statistics on Congress.\nNote: Average Term Length of all members of Congress in that particular congressional session.",
-       y = "Avg. Term Length", x = "Congress") +
+  labs(caption = "Damon Roberts | Data Source: Brookings Institute Vital Statistics on Congress.\nNote: Average Term Length of all members of Congress in that particular congressional session.", "Senate data divided by 3 to account for term length differences of House and Senate",
+       y = "Avg. Num of Terms", x = "Congress") +
   scale_x_continuous(breaks = round(seq(min(brookingsHOR$Congress), max(brookingsHOR$Congress), by = 4), 1))
 termtimeplot
 ggsave('figures/term-time-plot.png')
--- code/8_avg-seniority-hor-macro.R
+++ code/8_avg-seniority-hor-macro.R
@@ -11,20 +11,47 @@
 here::here()
 source('code/1_setup-file.R')
 setup()
-volden <- read.csv('data/volden-recoded-HOR.csv')
+volden <- read.csv('data/volden-recoded-HOR.csv') %>%
+  mutate(senior = yearCongStart - yearStarrt)
 #### Code ####
-volden <- volden %>%
+voldenf <- volden %>%
   filter(speaker == 1 | chair == 1 | chair_intell == 1 | majLeadership == 1 | minLeadership == 1) %>%
   mutate(senior = yearCongStart - yearStarrt)
-horaggage <- ddply(volden, .(congress), summarize, Seniority = mean(senior))
+voldenR <- volden %>%
+  filter(dem == 0)
+voldenD <- volden %>%
+  filter(dem == 1)
+voldenfR <- voldenf %>%
+  filter(dem == 0)
+voldenfD <- voldenf %>%
+  filter(dem==1)
+allseniorityR <- voldenR %>%
+  ddply(.(congress),summarize,allseniority = mean(senior))
+allseniorityD <- voldenD %>%
+  ddply(.(congress),summarize,allseniority = mean(senior))
+horaggageR <- ddply(voldenfR, .(congress), summarize, Seniority = mean(senior))
+horaggageRm <- left_join(horaggageR, allseniorityR, by="congress")  %>%
+  mutate(stdseniority = (Seniority/allseniority))
+horaggageD <- ddply(voldenfD, .(congress), summarize, Seniority = mean(senior))
+horaggageDm <- left_join(horaggageD, allseniorityD, by="congress") %>%
+  mutate(stdseniority = (Seniority/allseniority))
 
+annotatestdsenior <- 
+  data.frame(
+    x = c(95,94),
+    y = c(2.3,1.4),
+    label = c('Republican','Democrat')
+  )
 aggageplot <- ggplot() +
-  geom_line(data = horaggage, aes(x = congress, Seniority), color='#000000', size=2, alpha=0.9, linetype=4) +
+  geom_line(data = horaggageRm, aes(x = congress, y = stdseniority), color='#000000', size=2, alpha=0.9, linetype=1) +
+  geom_line(data = horaggageDm, aes(x = congress, y = stdseniority), color='#000000',size=2, alpha=0.9, linetype=2) +
   theme_tufte() +
-  ggtitle("Figure 3. Average Seniority - House") +
-  labs(caption = "Damon Roberts | Data sources: Volden and Wiseman Legislative Effectiveness Data - House data from 1973-2018.\nNotes: Data is constrained only to those who hold a subcommittee or committee chair position, a majority or minority leadership position, \nor a seat on the Appropriations committee.\nAverage Seniority is an average calculated from subtracting the current term's first calendar year from the member's first term's first calendar year in Congress.",
+  ggtitle("Figure 3. Average Seniority by Party- House") +
+  labs(caption = "Damon Roberts | Data sources: Volden and Wiseman Legislative Effectiveness Data - House data from 1973-2018.\nNotes: Data is constrained only to those who hold a subcommittee or committee chair position, a majority or minority leadership position, \nor a seat on the Appropriations committee.\nAverage Seniority is an average calculated from subtracting the current term's first calendar year from the member's first term's first calendar year in Congress \nthen standardized from the avg. seniority of all copartisan colleagues.", 
        x = "Congress", y = "Avg. Seniority") +
-  scale_x_continuous(breaks = round(seq(min(horaggage$congress), max(horaggage$congress), by = 4),1))
+  scale_x_continuous(breaks = round(seq(min(horaggage$congress), max(horaggage$congress), by = 4),1)) + 
+  geom_text(data =annotatestdsenior, aes(x=x, y=y, label=label),
+            color = '#000000',size = 5, frontface='bold')
 aggageplot
 ggsave('figures/avg-house-seniority-macro.png')
  
